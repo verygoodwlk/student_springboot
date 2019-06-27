83a398ff42102f49a0e7af7985002f17421b20d9
ken <123@qq.com>
2019-06-27 15:23:21 +0800

    修改了某些班级的功能

--- src/main/java/com/qf/controller/ClassController.java
+++ src/main/java/com/qf/controller/ClassController.java
@@ -1,9 +1,12 @@
 package com.qf.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @version 1.0
  * @user ken
  * @date 2019/6/27 15:19
  */
+@Controller
 public class ClassController {
 }
