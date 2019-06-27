4acdf03e232a57f44002f38fad9871860aabefa9
ken <123@qq.com>
2019-06-27 15:16:54 +0800

    实现学生信息修改的功能

--- src/main/java/com/qf/controller/StuController.java
+++ src/main/java/com/qf/controller/StuController.java
@@ -7,6 +7,7 @@ import com.qf.service.IStuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
